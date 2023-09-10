72aed2ff290ecc2b70cee1af42937ba805ccf1ba
GoldenEdit <sammyhmatthews@gmail.com>
2023-09-10 13:27:18 +0100

    Import make_response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, make_response, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
