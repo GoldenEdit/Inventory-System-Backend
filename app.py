2afb212bed0043d6a6e9c92ba2cb190ba2f5eea4
GoldenEdit <sammyhmatthews@gmail.com>
2023-09-11 09:25:31 +0100

    remove duplicate

--- app.py
+++ app.py
@@ -228,7 +228,6 @@ class Role(db.Model):
 
 @app.after_request
 def after_request(response):
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   response.headers.add('Access-Control-Allow-Credentials', 'true')
   print(request.headers)
