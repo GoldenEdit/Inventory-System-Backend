c5fd50754c7f85e292f8741ac611e191834843ad
GoldenEdit <sammyhmatthews@gmail.com>
2023-09-20 19:52:33 +0100

    Fixed KeyError in CRUDUser.update method

--- app/crud/crud_user.py
+++ app/crud/crud_user.py
@@ -26,16 +26,20 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
 
     def update(
         self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]
-    ) -> User:
-        if isinstance(obj_in, dict):
-            update_data = obj_in
-        else:
-            update_data = obj_in.dict(exclude_unset=True)
-        if update_data["password"]:
-            hashed_password = get_password_hash(update_data["password"])
-            del update_data["password"]
-            update_data["hashed_password"] = hashed_password
-        return super().update(db, db_obj=db_obj, obj_in=update_data)
+        ) -> User:
+            if isinstance(obj_in, dict):
+                update_data = obj_in
+            else:
+                update_data = obj_in.dict(exclude_unset=True)
+            
+            # Check for the existence of "password" key
+            if "password" in update_data:
+                hashed_password = get_password_hash(update_data["password"])
+                del update_data["password"]
+                update_data["hashed_password"] = hashed_password
+                
+            return super().update(db, db_obj=db_obj, obj_in=update_data)
+
 
     def authenticate(self, db: Session, *, email: str, password: str) -> Optional[User]:
         user = self.get_by_email(db, email=email)
