52137b52da4f0df420fe693e07a346bb639ed8b5
GoldenEdit <sammyhmatthews@gmail.com>
2023-09-21 01:15:07 +0100

    auto migrate alembic tables

--- pre-start.sh
+++ pre-start.sh
@@ -1,14 +1,13 @@
 #! /usr/bin/env bash
 
 # Let the DB start
- 
 python ./app/backend_pre_start.py
 
-# Run migrations
+# Generate new migration files if there are schema changes
+alembic revision --autogenerate -m "Auto-generated migration"
 
- 
+# Run migrations
 alembic upgrade head
- 
 
 # Create initial data in DB
 python ./app/initial_data.py
