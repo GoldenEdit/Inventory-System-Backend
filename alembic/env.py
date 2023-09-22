a00a5e63690a8b8f8e50b95a902bef242e86ad61
GoldenEdit <sammyhmatthews@gmail.com>
2023-09-22 11:25:21 +0100

    Heroku Postgres

--- alembic/env.py
+++ alembic/env.py
@@ -38,8 +38,12 @@ def get_url():
     # password = os.getenv("POSTGRES_PASSWORD", "")
     # server = os.getenv("POSTGRES_SERVER", "db")
     # db = os.getenv("POSTGRES_DB", "app")
+    
+    url = os.getenv("DATABASE_URL")
+    # if not url:
+    #     return f"postgresql://{user}:{password}@{server}/{db}"
 
-    return os.getenv("DATABASE_URL")
+    return url
 
 
 def run_migrations_offline():
