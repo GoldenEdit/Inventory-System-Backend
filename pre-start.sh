3d7f285ef2e1906e6e0356dc8bf8be2265222bf4
GoldenEdit <sammyhmatthews@gmail.com>
2023-09-22 10:36:53 +0100

    Update pre-start.sh

--- pre-start.sh
+++ pre-start.sh
@@ -1,13 +1,21 @@
 #! /usr/bin/env bash
 
+echo "Starting pre-start.sh script..."
+
 # Let the DB start
+echo "Running backend_pre_start.py..."
 python ./app/backend_pre_start.py
 
 # Generate new migration files if there are schema changes
+echo "Generating new migration files..."
 alembic revision --autogenerate -m "Auto-generated migration"
 
 # Run migrations
+echo "Running migrations..."
 alembic upgrade head
 
 # Create initial data in DB
+echo "Creating initial data in DB..."
 python ./app/initial_data.py
+
+echo "Exiting pre-start.sh script."
