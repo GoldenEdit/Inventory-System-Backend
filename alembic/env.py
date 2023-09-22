341a26e60acaf8bba6051a8c12a1be8cbfcc554e
GoldenEdit <sammyhmatthews@gmail.com>
2023-09-22 11:48:19 +0100

    Update env.py

--- alembic/env.py
+++ alembic/env.py
@@ -38,12 +38,17 @@ def get_url():
     # password = os.getenv("POSTGRES_PASSWORD", "")
     # server = os.getenv("POSTGRES_SERVER", "db")
     # db = os.getenv("POSTGRES_DB", "app")
-    
-    url = os.getenv("DATABASE_URL")
-    # if not url:
-    #     return f"postgresql://{user}:{password}@{server}/{db}"
 
-    return url
+    # url = os.getenv("DATABASE_URL")
+    # # if not url:
+    # #     return f"postgresql://{user}:{password}@{server}/{db}"
+
+    # return url
+    url = os.getenv("DATABASE_URL")
+    if url:
+        return url.replace("postgres://", "postgresql://", 1)
+    else:
+        return None  # or some default value
 
 
 def run_migrations_offline():
